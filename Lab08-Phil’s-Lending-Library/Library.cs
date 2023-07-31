4e4d57bd24313bcd8f202483cd886e8e76b3ef4e
ConnerKT <connerthompson121@gmail.com>
2023-07-31 11:18:19 -0500

    added function

--- Lab08-Phil’s-Lending-Library/Library.cs
+++ Lab08-Phil’s-Lending-Library/Library.cs
@@ -5,20 +5,24 @@ namespace Lab08_Phil_s_Lending_Library
 	{
 		private Dictionary<string, Book> Storage;
 
-		public Library()
-		{
+        public Library()
+        {
             //void Add(string title, string firstName,
-			//string lastName, int numberOfPages);
+            //string lastName, int numberOfPages);
             Book Book1 = new Book("To Killer a MockingBird", "Harper Lee", 100);
             Book Book2 = new Book("Americana", "Shemamonda", 100);
             Book Book3 = new Book("Berserk", "Kentaro Miura", 100);
 
-			Storage.Add(Book1.Title, Book1);
+            Storage.Add(Book1.Title, Book1);
             Storage.Add(Book2.Title, Book2);
             Storage.Add(Book3.Title, Book3);
 
         }
-		
-	}
+            public void Add(string title, string firstName, string lastName, int numberOfPages)
+            {
+
+            }
+
+        }
 }
 
